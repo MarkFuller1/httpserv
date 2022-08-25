96d9a1720fc97f2f8805f09aa78aee59c5205691
Mark Fuller <Mark_Fuller1@baylor.edu>
2022-08-25 13:11:50 -0700

    working tcp echo server

--- src/TCP_Server.cpp
+++ src/TCP_Server.cpp
@@ -15,33 +15,27 @@
 
 TCP_Server *TCP_Server::instance;
 
-TCP_Server *TCP_Server::getInstance()
-{
-    if (TCP_Server::instance == NULL)
-    {
-        std::cout << "Building server" << std::endl;
-        TCP_Server serv = TCP_Server();
-        TCP_Server::instance = &serv;
+TCP_Server *TCP_Server::getInstance() {
+    if (TCP_Server::instance == NULL) {
+        TCP_Server::instance = new TCP_Server();
     }
-    std::cout << "returning server" << std::endl;
     return instance;
 }
 
-TCP_Server::TCP_Server()
-{
+TCP_Server::TCP_Server() {
+    message = new char[TCP_Server::MESSAGE_SIZE];
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("In socket");
-        delete[] message;
+        this->~TCP_Server();
         exit(EXIT_FAILURE);
     }
 
     linger lin;
     lin.l_onoff = 0;
     lin.l_linger = 0;
-    setsockopt(server_fd, SOL_SOCKET, SO_LINGER, (const char *)&lin, sizeof(int));
+    setsockopt(server_fd, SOL_SOCKET, SO_LINGER, (const char *) &lin, sizeof(int));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
@@ -51,83 +45,64 @@ TCP_Server::TCP_Server()
 
     addrlen = sizeof(address);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("In bind");
-        delete[] message;
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 10) < 0)
-    {
         perror("In listen");
-        delete[] message;
+        instance->~TCP_Server();
         exit(EXIT_FAILURE);
     }
-    
+    if (listen(server_fd, 10) < 0) {
+        perror("In listen");
+        instance->~TCP_Server();
+        exit(EXIT_FAILURE);
+    }
+
     std::cout << toString() << std::endl;
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-    {
+}
+
+char *TCP_Server::connect() {
+    printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+    std::cout << toString() << std::endl;
+    if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
         perror("In accept");
 
-        delete[] message;
+        instance->~TCP_Server();
         exit(EXIT_FAILURE);
     }
-
-    std::cout << toString() << std::endl;
-}
-
-void TCP_Server::sockRead()
-{
-
-    std::cout << "reading from socket" << std::endl;
-    valread = read(new_socket, message, MESSAGE_SIZE);
-
-    if (valread < 1)
-    {
-        std::cout << "an error occurred:" << valread << std::endl;
-        return;
-    }
-    std::cout << "read" << std::string(message) << std::endl;
-}
-
-char *TCP_Server::connect()
-{
-    printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-    // std::cout << toString() << std::endl;
-    // if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-    // {
-    //     perror("In accept");
-
-    //     delete[] message;
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // sockRead();
 
     return message;
 }
 
-void TCP_Server::respond(const char *data, int size)
-{
-    write(new_socket, data, size);
+void TCP_Server::sockRead() {
+
+    std::cout << "reading from socket" << std::endl;
+    valread = read(new_socket, message, MESSAGE_SIZE);
+
+    if (valread < 1) {
+        std::cout << "an error occurred:" << valread << std::endl;
+        return;
+    }
+    std::cout << "read:\t" << std::string(message, valread) << std::endl;
 }
 
-bool TCP_Server::disconnect()
-{
+void TCP_Server::respond(const char *data, int size) {
+    write(instance->new_socket, data, size);
+}
+
+bool TCP_Server::disconnect() {
     close(new_socket);
     new_socket = -1;
 
     std::cout << "disconnected from socket" << std::endl;
 
-    delete[] message;
+    instance->~TCP_Server();
     std::cout << "deallocated memory" << std::endl;
 
     return true;
 }
 
-bool TCP_Server::isClosed()
-{
-    std::cout << "checking if socket is closed" << std::endl;
+bool TCP_Server::isClosed() {
+    std::cout << "-- checking if socket is closed" << std::endl;
     fd_set rfd;
     FD_ZERO(&rfd);
     FD_SET(new_socket, &rfd);
@@ -135,30 +110,56 @@ bool TCP_Server::isClosed()
 
     select(new_socket + 1, &rfd, 0, 0, &tv);
 
-    if (!FD_ISSET(new_socket, &rfd))
-    {
-        std::cout << "socket is not closed" << std::endl;
+    if (!FD_ISSET(new_socket, &rfd)) {
+        std::cout << "-- socket is not closed" << std::endl;
         return false;
     }
 
     int n = 0;
     ioctl(new_socket, FIONREAD, &n);
 
-    std::cout << "socket is " << (n == 0 ? " closed" : " not closed") << std::endl;
+    std::cout << "-- socket is " << (n == 0 ? "closed" : "not closed") << std::endl;
 
     return n == 0;
 }
 
-std::string TCP_Server::toString()
-{
+std::string TCP_Server::toString() {
     std::stringstream ss;
 
     ss << "{ PORT:" << PORT << ",";
     ss << "MESSAGE_SIZE:" << MESSAGE_SIZE << ",";
-    ss << "server_fd:" << server_fd << ",";
-    ss << "new_socket:" << new_socket << ",";
-    ss << "message:" << message << ",";
+    ss << "server_fd:" << g_server_fd() << ",";
+    ss << "new_socket:" << g_new_socket() << ",";
+    ss << "message:" << g_message() << ",";
     ss << "valread:" << valread << "}";
 
     return ss.str();
 }
+
+int TCP_Server::g_server_fd() {
+    return TCP_Server::server_fd;
+}
+
+int TCP_Server::g_new_socket() {
+    return TCP_Server::new_socket;
+}
+
+long TCP_Server::g_valread() {
+    return TCP_Server::valread;
+}
+
+sockaddr_in TCP_Server::g_address() {
+    return TCP_Server::address;
+}
+
+std::string TCP_Server::g_message() {
+    return std::string(TCP_Server::message, TCP_Server::valread);
+}
+
+TCP_Server::~TCP_Server() {
+    std::cout << "Shutting down server gracefully" << std::endl;
+    delete[] message;
+}
+
+
+
--- src/include/TCP_Server.h
+++ src/include/TCP_Server.h
@@ -6,17 +6,17 @@
 class TCP_Server
 {
 public:
-    const int PORT = 8080;
-    const int MESSAGE_SIZE = 30000;
+    static const int PORT = 8082;
+    static const int MESSAGE_SIZE = 30000;
 
-    int server_fd = -1, new_socket = -1;
-    struct sockaddr_in address;
-    char *message = new char[MESSAGE_SIZE];
-
-    long valread = 0;
-    long addrlen = 0;
+    int g_server_fd();
+    int g_new_socket();
+    sockaddr_in g_address();
+    std::string g_message();
+    long g_valread();
 
     static TCP_Server *getInstance();
+    ~TCP_Server();
     char *connect();
     void sockRead();
     void respond(const char *data, int size);
@@ -27,4 +27,11 @@ public:
 private:
     TCP_Server();
     static TCP_Server *instance;
+
+    int server_fd = -1, new_socket = -1;
+    struct sockaddr_in address;
+    char *message;
+
+    long valread = 0;
+    long addrlen = 0;
 };
